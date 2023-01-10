2a63e7c924dfa7d2b7f0ce662c1baf187dadbf94
Ragnar <ragnar.naruski@voco.ee>
2023-01-10 21:08:58 +0200

    split custom component into multiple

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,14 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
+
 
 function ExpenseItem(props){
-    const month=props.expenseData.date.toLocaleString('en-US',{month:'long'})
-    const day=props.expenseData.date.toLocaleString('en-US',{day:'2-digit'})
-    const year=props.expenseData.date.getFullYear()
+
 
     return (
         <div className='expense-item'>
-            <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
-            </div>
+            <ExpenseDate date={props.expenseData.date}></ExpenseDate>
 
-            <div>{props.expenseData.date.toISOString()}</div>
             <div className='expense-item__description'>
                 <h2>{props.expenseData.title}</h2>
                 <div className='expense-item__price'>
