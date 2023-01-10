e608f39556b0e3853fa2d44280b77ff55ea27fcc
Ragnar <ragnar.naruski@voco.ee>
2023-01-10 09:56:15 +0200

    Created first custom component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <div>Date</div>
+            <div>
+                <div>Title</div>
+                <div>Amount</div>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default ExpenseItem
