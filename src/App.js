ff0bf1d066dd039cb1715fa2d316d6210fd24984
Ragnar <ragnar.naruski@voco.ee>
2023-01-18 15:33:49 +0200

    Added newExpense

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = ()=> {
 
   return (
     <div className="App">
+        <NewExpense></NewExpense>
         <Expenses></Expenses>
 
     </div>
