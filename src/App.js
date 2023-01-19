48060b0549820ad5f85a9e7fb1bb497831dc65be
Ragnar <ragnar.naruski@voco.ee>
2023-01-19 10:54:32 +0200

    Bottom-up

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = ()=> {
-
+    const addExpensehandler=(expense)=>{
+        console.log('In App.js')
+        console.log(expense)
+    }
   return (
     <div className="App">
-        <NewExpense></NewExpense>
+        <NewExpense onAddExpense={addExpensehandler}></NewExpense>
         <Expenses></Expenses>
 
     </div>
