ebfc08836e69dd7cb6e7da79ae1e45e210b57175
Ragnar <ragnar.naruski@voco.ee>
2023-03-08 14:04:43 +0200

    Added some more to the test

--- cypress/e2e/expenses.cy.js
+++ cypress/e2e/expenses.cy.js
@@ -6,26 +6,33 @@ describe('Expenses', () => {
 
   it('Open form and add a new expense',()=>{
 
+    cy.get('.expense-item').should('have.length',1)
     cy.contains('Add new Expense').click()
     cy.get('input[type="text"]') .type('expense')
     cy.get('input[type="number"]').type('22.7')
     cy.get('input[type="date"]') .type('2023-01-19')
     cy.contains('Add Expense').click()
+    cy.get('.expense-item').should('have.length',2)
+
   })
 
   it('Add another expense to 2024 and filter expenses by 2024',()=>{
 
+    cy.get('.expense-item').should('have.length',1)
     cy.contains('Add new Expense').click()
     cy.get('input[type="text"]') .type('expense 2')
     cy.get('input[type="number"]').type('23')
     cy.get('input[type="date"]') .type('2024-05-19')
     cy.contains('Add Expense').click()
     cy.get('select').select('2024')
+    cy.get('.expense-item').should('have.length',2)
   })
 
   it('Check if cancel button works', ()=> {
 
     cy.contains('Add new Expense').click()
     cy.contains('Cancel').click()
+    cy.contains('Add new Expense').should("exist")
+
   });
 })
